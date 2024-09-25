be522e26d4dc4942794593e01d605eead5a4f9fb
rosroble <bprnd2@yandex.ru>
2024-09-25 19:13:15 +0300

    add space handling in name types

--- 1/func.cpp
+++ 1/func.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 void f(void) {
-	int int_0 = 1;
+	long long long_long_0 = 1;
 	float float_1 = 2;
-	std::cout << int_0 + float_1 << std::endl;
-	std::cout << int_0 - float_1 << std::endl;
+	std::cout << long_long_0 + float_1 << std::endl;
+	std::cout << long_long_0 - float_1 << std::endl;
 }
 int main() { f(); return 0; }
--- 1/main.cpp
+++ 1/main.cpp
@@ -5,16 +5,22 @@
 
 using namespace std;
 
+string space2underscore(std::string text)
+{
+    replace(text.begin(), text.end(), ' ', '_');
+    return text;
+}
+
 void gen(vector<string> &types, vector<string> &opers, ofstream &fout) {
     fout << "#include <iostream>" << endl;
 	fout << "void f(void) {"<< endl;
     for (int i = 0; i < types.size(); i++) {
-        fout << "\t" << types[i] << " " << types[i] << "_" << i << " = " << i + 1 << ";" << endl;
+        fout << "\t" << types[i] << " " << space2underscore(types[i]) << "_" << i << " = " << i + 1 << ";" << endl;
     }
     for (int i = 0; i < opers.size(); i++) {
         fout << "\t" << "std::cout << ";
         for (int j = 0; j < types.size() - 1; j++) {
-            fout << types[j] << "_" << j << " " << opers[i] << " ";
+            fout << space2underscore(types[j]) << "_" << j << " " << opers[i] << " ";
         }
         fout << types[types.size() - 1] << "_" << types.size() - 1 << " << std::endl" << ";" << endl;
     }
@@ -32,11 +38,13 @@ int main() {
         cout << "Bad args: n must be > 0";
         return -1;
     }
-
+    
     vector<string> types;
+    cin.ignore();
     for (int i = 0; i < n; i++) {
+        cout << "-----" << i << endl;
         string type;
-        cin >> type;
+        getline(cin, type);
         types.push_back(type);
     }
 
@@ -46,10 +54,12 @@ int main() {
         cout << "Bad args: n must be > 0";
         return -1;
     }
+    
     vector<string> opers;
+    cin.ignore();
     for (int i = 0; i < n; i++) {
         string oper;
-        cin >> oper;
+        getline(cin, oper, '\n');
         opers.push_back(oper);
     }
 
